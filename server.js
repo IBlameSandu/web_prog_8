c624df47e5594fb200dc96b42612490f8f9fe411
IBlameSandu <127618863+TheOneAugust@users.noreply.github.com>
2024-04-19 14:48:25 +0200

    a

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const server = express();
 
-server.get("/test", function () {
-  console.log("working");
+const tests = [
+  {
+    id: 1,
+    task: "procrastinate",
+    checked: false,
+  },
+  {
+    id: 2,
+    task: "eat",
+    checked: true,
+  },
+  {
+    id: 3,
+    task: "sleep",
+    checked: false,
+  },
+];
+
+server.get("/tests", function (req, res) {
+  res.send(tests);
 });
 //server.post();
 //server.put();
 //server.delete();
 
 server.listen(1234, function () {
-  console.log(" 🤓 your server is now listening on http://localhost:1234");
+  console.log("your server is now listening on http://localhost:1234");
 });
