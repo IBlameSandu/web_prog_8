aab9b97b008d5037b372e46c56f78f0148f27d79
IBlameSandu <127618863+TheOneAugust@users.noreply.github.com>
2024-04-19 14:29:11 +0200

    get function

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const server = express();
 
+server.get("/test", function () {
+  console.log("working");
+});
+//server.post();
+//server.put();
+//server.delete();
+
 server.listen(1234, function () {
   console.log(" 🤓 your server is now listening on http://localhost:1234");
 });
